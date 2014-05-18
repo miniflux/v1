59ba6bbed44a43a3e00b9a68978a7a57d2c97973
Frédéric Guillot <fred@miniflux.net>
2014-05-18 10:40:59 -0400

    Add meta tag apple-mobile-web-app-capable

--- templates/layout.php
+++ templates/layout.php
@@ -2,15 +2,19 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <title><?= isset($title) ? Helper\escape($title) : 'Miniflux' ?></title>
+
         <meta name="viewport" content="width=device-width">
         <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+
         <link rel="icon" type="image/png" href="assets/img/favicon.png">
         <link rel="shortcut icon" href="favicon.ico">
         <link rel="apple-touch-icon" href="assets/img/touch-icon-iphone.png">
         <link rel="apple-touch-icon" sizes="72x72" href="assets/img/touch-icon-ipad.png">
         <link rel="apple-touch-icon" sizes="114x114" href="assets/img/touch-icon-iphone-retina.png">
         <link rel="apple-touch-icon" sizes="144x144" href="assets/img/touch-icon-ipad-retina.png">
-        <title><?= isset($title) ? Helper\escape($title) : 'Miniflux' ?></title>
+
         <link href="<?= Helper\css() ?>" rel="stylesheet" media="screen">
         <script type="text/javascript" src="assets/js/all.min.js?<?= filemtime('assets/js/all.min.js') ?>" defer></script>
     </head>
