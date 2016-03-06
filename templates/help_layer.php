08ea480402058efe5bbc128fe0bff67847b2f375
denfil <denis@fdesign.ru>
2016-03-06 18:23:19 +0300

    Replace help window by layer

---
+++ templates/help_layer.php
@@ -0,0 +1,6 @@
+<div id="help-layer" class="hide">
+    <span class="btn-close" data-action="close-help">&times;</span>
+    <section>
+        <?= \Template\load('keyboard_shortcuts') ?>
+    </section>
+</div>
