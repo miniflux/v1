b0ba363871a5135fdf97438faea7573df7a37da6
Frederic Guillot <fred@miniflux.net>
2016-03-17 19:57:46 -0400

    Swap unread icons and remove useless href attributes

--- templates/bookmark_links.php
+++ templates/bookmark_links.php
@@ -1,7 +1,6 @@
 <?php if ($item['bookmark']): ?>
     <span
         class="bookmark-icon"
-        href="?action=bookmark&amp;value=0&amp;id=<?= $item['id'] ?>&amp;menu=<?= $menu ?>&amp;offset=<?= $offset ?>&amp;source=<?= $source ?>"
         data-action="bookmark"
         data-reverse-title="<?= t('bookmark') ?>"
         title="<?= t('remove bookmark') ?>"
@@ -9,7 +8,6 @@
 <?php else: ?>
     <span
         class="bookmark-icon"
-        href="?action=bookmark&amp;value=1&amp;id=<?= $item['id'] ?>&amp;menu=<?= $menu ?>&amp;offset=<?= $offset ?>&amp;source=<?= $source ?>"
         data-action="bookmark"
         data-reverse-title="<?= t('remove bookmark') ?>"
         title="<?= t('bookmark') ?>"
--- templates/status_links.php
+++ templates/status_links.php
@@ -1,7 +1,6 @@
 <?php if ($item['status'] == 'unread'): ?>
     <span
         class="read-icon"
-        href="?action=mark-item-read&amp;id=<?= $item['id'] ?>&amp;offset=<?= $offset ?>&amp;redirect=<?= $redirect ?>&amp;feed_id=<?= $item['feed_id'] ?>"
         data-action="mark-read"
         data-reverse-title="<?= t('mark as unread') ?>"
         title="<?= t('mark as read') ?>"
@@ -9,7 +8,6 @@
 <?php else: ?>
     <span
         class="read-icon"
-        href="?action=mark-item-unread&amp;id=<?= $item['id'] ?>&amp;offset=<?= $offset ?>&amp;redirect=<?= $redirect ?>&amp;feed_id=<?= $item['feed_id'] ?>"
         data-action="mark-unread"
         data-reverse-title="<?= t('mark as read') ?>"
         title="<?= t('mark as unread') ?>"
