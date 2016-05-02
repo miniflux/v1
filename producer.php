d0fda5f1b465754bad0cb828ed42cd28feb5195a
Frederic Guillot <fred@miniflux.net>
2016-05-01 22:32:50 -0400

    Added limit option for producer

--- producer.php
+++ producer.php
@@ -8,9 +8,14 @@ if (php_sapi_name() !== 'cli') {
     die('This script can run only from the command line.'.PHP_EOL);
 }
 
+$options = getopt('', array(
+    'limit::',
+));
+
+$limit = ! empty($options['limit']) && ctype_digit($options['limit']) ? (int) $options['limit'] : Model\Feed\LIMIT_ALL;
 $connection = new Pheanstalk(BEANSTALKD_HOST);
 
-foreach (Model\Feed\get_ids() as $feed_id) {
+foreach (Model\Feed\get_ids($limit) as $feed_id) {
     $connection
         ->useTube(BEANSTALKD_QUEUE)
         ->put($feed_id, Pheanstalk::DEFAULT_PRIORITY, Pheanstalk::DEFAULT_DELAY, BEANSTALKD_TTL);
