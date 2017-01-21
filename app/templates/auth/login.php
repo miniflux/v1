7d6fc3336bad6e9244e8370a7026fa989cf28697
Frederic Guillot <fred@miniflux.net>
2017-01-21 14:44:57 -0500

    Do not duplicate head tag

--- app/templates/auth/login.php
+++ app/templates/auth/login.php
@@ -5,20 +5,7 @@
     <?php endif ?>
 >
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="robots" content="noindex,nofollow">
-        <meta name="referrer" content="no-referrer">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <link rel="icon" type="image/png" href="assets/img/favicon.png">
-        <link rel="shortcut icon" href="favicon.ico">
-        <link rel="apple-touch-icon" href="assets/img/touch-icon-iphone.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="assets/img/touch-icon-ipad.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="assets/img/touch-icon-iphone-retina.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="assets/img/touch-icon-ipad-retina.png">
-        <title>Miniflux</title>
+        <?php echo Miniflux\Template\load('common/head') ?>
         <link href="<?php echo Miniflux\Helper\css() ?>" rel="stylesheet" media="screen">
     </head>
     <body id="login-page">
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -5,23 +5,7 @@
     <?php endif ?>
 >
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="robots" content="noindex,nofollow">
-        <meta name="referrer" content="no-referrer">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <title><?php echo isset($title) ? Miniflux\Helper\escape($title) : t('Miniflux') ?></title>
-
-        <link rel="icon" type="image/png" href="assets/img/favicon.png">
-        <link rel="shortcut icon" href="favicon.ico">
-        <link rel="apple-touch-icon" href="assets/img/touch-icon-iphone.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="assets/img/touch-icon-ipad.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="assets/img/touch-icon-iphone-retina.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="assets/img/touch-icon-ipad-retina.png">
-
-        <link href="<?php echo Miniflux\Helper\css() ?>" rel="stylesheet" media="screen">
+        <?php echo Miniflux\Template\load('common/head') ?>
         <script type="text/javascript" src="assets/js/app.min.js?<?php echo filemtime('assets/js/app.min.js') ?>" defer></script>
     </head>
     <body>
